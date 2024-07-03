bb33d715c6f0b5411579cf53671163379bd87c86
MC1398 <matiascerruti13@gmail.com>
2024-07-03 19:44:06 -0400

    task9

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,11 +1,12 @@
 const search = document.getElementById('search');
 const clear = document.getElementById('clear');
 const reco = document.getElementById('reco');
+const input = document.getElementById('destination');
 
 
 function recoResults() {
-    const destination = document.getElementById('destination').value.toLowerCase();
-    console.log(destination)
+    const destination = input.value.toLowerCase();
+    
     let finalDestination = ''
     
     if(destination == 'beach' || destination == 'beaches'){
@@ -16,27 +17,36 @@ function recoResults() {
         finalDestination = 'countries'
     };
 
-    console.log(finalDestination)
     
     fetch('./travel_recommendation_api.json')
         .then(response => response.json())
-        .then(data => {           
-            console.log(data.beaches[0])
-            console.log(finalDestination)
-            console.log(data.finalDestination[0])
-            // if(finalDestination == 'beaches' || finalDestination == 'temples'){
-            //     reco.innerHTML = `${data.finalDestination[0].name}`
-
-
-                
-                
-            // } else {
-
-            // }
+        .then(data => {
             
-        })
-        
+            if(finalDestination == 'beaches'){
+                reco.innerHTML = `<figure><img src="${data.beaches[0].imageUrl}" width="600" height="270">
+                <figcaption><b>${data.beaches[0].name}</b> <br> ${data.beaches[0].description}</figcaption></figure> <br>
+                <figure><img src="${data.beaches[1].imageUrl}" width="600" height="270">
+                <figcaption><b>${data.beaches[1].name}</b> <br> ${data.beaches[1].description}</figcaption></figure>`
+                                
+            } else  if(finalDestination == 'temples'){ 
+                reco.innerHTML = `<figure><img src="${data.temples[0].imageUrl}" width="600" height="270">
+                <figcaption><b>${data.temples[0].name}</b> <br> ${data.temples[0].description}</figcaption></figure> <br>
+                <figure><img src="${data.temples[1].imageUrl}" width="600" height="270">
+                <figcaption><b>${data.temples[1].name}</b> <br> ${data.temples[1].description}</figcaption></figure>`
 
+            } else {
+                reco.innerHTML = `<figure><img src="${data.countries[0].cities[0].imageUrl}" width="600" height="270">
+                <figcaption><b>${data.countries[0].cities[0].name}</b> <br> ${data.countries[0].cities[0].description}</figcaption></figure> <br>
+                <figure><img src="${data.countries[0].cities[1].imageUrl}" width="600" height="270">
+                <figcaption><b>${data.countries[0].cities[1].name}</b> <br> ${data.countries[0].cities[1].description}</figcaption></figure>`
+            }            
+        })        
+};
+
+function clearResult() {
+    reco.innerHTML = '';
+    input.value = '';
 }
 
-search.addEventListener('click', recoResults)
+search.addEventListener('click', recoResults);
+clear.addEventListener('click', clearResult)
